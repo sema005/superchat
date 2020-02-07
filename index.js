b1175973448040dad49f49c185d741003d80a39e
Sebastian <sebastianmar00@gmail.com>
2020-02-07 10:24:37 +0100

    changes

--- index.js
+++ index.js
@@ -1,15 +1,49 @@
 
-
-
-const user = prompt("Hva heter du?");
+//const user = prompt("Hva heter du?");
+const user = "Sebastian"
 // HTML-elementer
 const messageForm = document.querySelector("#messageForm");
 const messageText = document.querySelector("#messageText");
 const messageDiv = document.querySelector("#messageDiv");
+const inpFarge = document.querySelector("#inpFarge");
+const main = document.querySelector("main");
+
+
+
 
 //Lager databasen
 const db = firebase.firestore();
 const chatten = db.collection("chatten");
+
+const prikker = db.collection("prikker");
+
+
+
+document.onclick = (evt) => {
+    prikker.add({
+        x: evt.clientX,
+        y: evt.clientY,
+        farge: inpFarge.value
+    });
+}
+
+prikker.onSnapshot(snap => {
+    for( const prikk of snap.docChanges() ) {
+        if(prikk.type === "added") {
+            const p = prikk.doc.data();
+            main.innerHTML += `
+                <div 
+                    class="prikk"
+                    style="left: ${p.x}px; top: ${p.y}px; background-color: ${p.farge}"
+                >
+                </div>
+            `;
+        }
+    }
+})
+
+
+
 
 chatten.onSnapshot(snap => {
     for (const message of snap.docChanges()) {
@@ -35,10 +69,8 @@ messageForm.onsubmit = (evt) => {
     chatten.add({
         from: user,
         text: messageText.value,
-        time: firebase
-            .firestore
-            .FieldValue
-            .serverTimestamp()
+        time: firebase.firestore.FieldValue.serverTimestamp()
     });
 
+    messageForm.reset();
 }
